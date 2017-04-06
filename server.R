1dd6b4392eb15e4905efcc9069ff54c24309467e
sfields1 <sfields1@live.maryville.edu>
2017-04-06 05:44:18 -0500

    Updated App

--- server.R
+++ server.R
@@ -1,14 +1,49 @@
-server <- function(input, output) {
+library(shiny)
+library(datasets)
+library(dplyr)
+library(tidyr)
+library(plotly)
+
+shinyServer(function(input, output) {
+
         
+
         
-        
-        rv <- reactiveValues(data = rnorm(100))
-        
-        observeEvent(input$norm, {rv$data <- rnorm(input$num)})
-        observeEvent(input$unif, {rv$data <- runif(input$num)})
-        
-        output$hist <- renderPlot({
-                hist(rv$data,
-                     main = isolate({input$title}))
-        })
-}
+      output$table <- renderDataTable({
+              
+              nottem <- tapply(nottem, list(year = floor(time(nottem)), month = month.name[cycle(nottem)]), c)
+              nottem <- as.data.frame(nottem)
+              nottem <- nottem %>%
+                      mutate(Year = rownames(nottem)) %>%
+                      select(Year, January, February, March, April, May, June, 
+                             July, August, September, October, November, December) %>%
+                      gather(Month, Temperature, -Year) %>%
+                      filter(Month %in% input$month, Year %in% input$year)
+              
+              
+              
+              nottem$Month <- factor(nottem$Month, level = unique(nottem$Month))
+              
+              nottem
+              
+      }, options = list(lengthMenu = c(5, 15, 25), pageLength = 25))
+      
+      output$bar <- renderPlotly({
+              
+              nottem <- tapply(nottem, list(year = floor(time(nottem)), month = month.name[cycle(nottem)]), c)
+              nottem <- as.data.frame(nottem)
+              nottem <- nottem %>%
+                      mutate(Year = rownames(nottem)) %>%
+                      select(Year, January, February, March, April, May, June, 
+                             July, August, September, October, November, December) %>%
+                      gather(Month, Temperature, -Year) %>%
+                      filter(Month %in% input$month, Year %in% input$year)
+              
+              
+              
+              nottem$Month <- factor(nottem$Month, level = unique(nottem$Month))
+              
+              plot_ly(nottem, x = ~Year, y = ~Temperature, color = ~Month, type = 'bar', colors = "Set3")
+      })
+})
+
