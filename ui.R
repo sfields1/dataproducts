feca3a5a46bf44e56dd15797c51bf40d4594ff0d
sfields1 <sfields1@live.maryville.edu>
2017-04-06 05:44:50 -0500

    Updated App

--- ui.R
+++ ui.R
@@ -1,19 +1,47 @@
-ui <- fluidPage(
-        sidebarLayout(
-                sidebarPanel = 
+library(shiny)
+library(datasets)
+library(dplyr)
+library(tidyr)
+library(plotly)
+
+shinyUI(navbarPage("Average Monthly Temperatures at Nottingham, 1920-1939",
+                   tabPanel("Table",
+                            
+                   sidebarLayout(
+                           sidebarPanel(
+                                   submitButton("Submit"),
+                                   helpText("Choose which months and year to include"),
+                                   checkboxGroupInput("month", "Month", choices = c("January", "February",
+                                                                                    "March", "April", "May", 
+                                                                                    "June", "July", "August",
+                                                                                    "September", "October",
+                                                                                    "November", "December"),
+                                                      selected = c("January", "February", "March", "April", "May",
+                                                      "June", "July", "August", "September", "October",
+                                                      "November", "December")),
+                                   checkboxGroupInput("year", "Year", choices = c(1920:1939),
+                                                      selected = c(1920:1939))
+                                   
+                                   ),
+                           
+                           mainPanel(
+                                   dataTableOutput("table")
+                                   )
+                           )
+                   ),
+                   
+                   tabPanel("Chart",
+                            plotlyOutput("bar")
+                   ),
+                   
+                   tabPanel("About",
+                            mainPanel(
+                                    print("This app uses data from the 'nottem' dataset which comes with base R.
+                                          The dataset contains average monthly temperatures at Nottingham
+                                          between the years of 1920 and 1939. The app itself allows the user 
+                                          to select what months and years to include in the dataset which is 
+                                          displayed on the 'Table' tab. There is a bar graph on the 'Chart'
+                                          tab that will update as different checkboxes are marked.")
+                            ))
         
-        sliderInput(inputId = "num",
-                    label = "Choose how large a sample to use",
-                    value = 100, min = 1, max = 1000),
-        
-        textInput(inputId = "title", 
-                  label = "Write a title",
-                  value = "Histogram of Normal or Uniform Numbers"),
-        ),
-        mainPanel(
-        actionButton(inputId = "norm", label = "Normal"),
-        actionButton(inputId = "unif", label = "Uniform"),
-        
-        plotOutput("hist")
-        )
-)
+))
